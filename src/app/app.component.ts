f97af460353228209efe0c28323ce8cbd37a9b8e
amanabiy <amanuelsisay.as@gmail.com>
2021-11-05 11:43:43 +0300

    front-end

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './shared/models/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Shopper';
+
+  constructor() { }
+
+  ngOnInit(): void {}
+}
---
+++ src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: IProduct;
+
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetail', '');
+     }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe( response => {
+      this.product = response;
+      this.bcService.set('@productDetails', response.name)
+    }, error => {
+      console.log(error);
+    })
+  }
+
+}
